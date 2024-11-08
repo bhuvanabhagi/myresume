1d3c0c539ed63c5bddda2588328a915f58273905
bhuvanabhagi <bhagibhuvaneswari@gmail.com>
2024-11-08 22:47:59 +0530

    Delete Project08_1 directory

--- Project08_1/App08_1/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns=[
-    path('add/', views.add, name='add'),          
-    path('calculate/', views.calculate, name='calculate')
-    
-]
